2c1ff42ebac1b9c1e6123856a91d4ba6121f34ac
mediocregopher <bgp2396@bellsouth.net>
2013-01-10 22:57:24 -0500

    made ssh use tty and not output so much shit

--- lib/ssh.py
+++ lib/ssh.py
@@ -6,7 +6,7 @@ def ssh(node,command,cwd="/tmp"):
     '''ssh's into a node and runs command, cd-ing into cwd first'''
 
     ssh_command = "cd {0} && {1}".format(cwd,command)
-    command = ["ssh","-p22","-i","snap_key","ccsnap@{0}".format(node["externalips"][0]),ssh_command]
+    command = ["ssh","-t","-q","-p22","-i","snap_key","ccsnap@{0}".format(node["externalips"][0]),ssh_command]
     print(str.join(" ",command[:-1])+" '{0}'".format(ssh_command))
 
     command_check_stderr(command)
